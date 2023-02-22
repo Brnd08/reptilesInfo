1924d92de04ef6f90e42cdbb4c47a5dfffd8e0c2
Brdn08 <93061195+Brnd08@users.noreply.github.com>
2023-02-21 23:59:43 -0600

    modify principal buttons id, create a on click listener using a lambda expresion and asign it to the four buttons

--- app/src/main/java/com/example/reptiles/MainActivity.java
+++ app/src/main/java/com/example/reptiles/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.reptiles;
 
+import static com.example.reptiles.R.id.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+    View.OnClickListener principalButtonsListener;
+
+    Button firstPrincipalButton;
+    Button secondPrincipalButton;
+    Button thirdPrincipalButton;
+    Button fourthPrincipalButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        principalButtonsListener = (View view) ->{
+                String buttonText = ((Button) view).getText().toString();
+                switch (buttonText){
+                    case "Cocodrilos":
+                        break;
+                    case "Squamatas":
+                        break;
+                    case "Quelenios":
+                        break;
+                    case "Binocéfalos":
+                        break;
+                    default:
+                        System.out.println("hi");
+            }
+        };
+
+        firstPrincipalButton = (Button)findViewById(firstButton);
+        secondPrincipalButton = (Button)findViewById(secondButton);
+        thirdPrincipalButton = (Button)findViewById(thirdButton);
+        fourthPrincipalButton = (Button)findViewById(fourthButton);
+
+        firstPrincipalButton.setOnClickListener(principalButtonsListener);
+        secondPrincipalButton.setOnClickListener(principalButtonsListener);
+        thirdPrincipalButton.setOnClickListener(principalButtonsListener);
+        fourthPrincipalButton.setOnClickListener(principalButtonsListener);
+
     }
+
+
+
 }
